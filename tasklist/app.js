97f6c90c8e872079fa6ea054742eb220821944df
Benjamin Benkert <ben@peakperformancepc.com>
2018-07-22 18:29:42 -0400

    Add filter function and event listener

--- tasklist/app.js
+++ tasklist/app.js
@@ -15,6 +15,8 @@ function loadEventListeners() {
   taskList.addEventListener('click', removeTask);
   //Clear task event
   clearBtn.addEventListener('click', clearTasks);
+  //Filter tasks
+  filter.addEventListener('keyup', filterTasks);
 }
 
 //Add Task
@@ -62,4 +64,18 @@ function clearTasks() {
       taskList.removeChild(taskList.firstChild);
     }
   }
+}
+
+//Filter tasks
+function filterTasks(e) {
+  const text = e.target.value.toLowerCase();
+
+  document.querySelectorAll('.collection-item').forEach(function(task) {
+    const item = task.firstChild.textContent;
+    if (item.toLowerCase().indexOf(text) != -1) {
+      task.style.display = 'block';
+    } else {
+      task.style.display = 'none';
+    }
+  });
 }
