5501ad8feb2d0cf105eb5ec816ed57d609eb61e3
Benjamin Benkert <ben@peakperformancepc.com>
2018-07-22 18:50:53 -0400

    Add function to show stored tasks

--- tasklist/app.js
+++ tasklist/app.js
@@ -9,6 +9,8 @@ const taskInput = document.querySelector('#task');
 loadEventListeners();
 
 function loadEventListeners() {
+  // DOM load event
+  document.addEventListener('DOMContentLoaded', getTasks);
   // Add task Event
   form.addEventListener('submit', addTask);
   //Remove task event
@@ -17,6 +19,35 @@ function loadEventListeners() {
   clearBtn.addEventListener('click', clearTasks);
   //Filter tasks
   filter.addEventListener('keyup', filterTasks);
+}
+
+//Get tasks from LS
+function getTasks() {
+  let tasks;
+  if (localStorage.getItem('tasks') === null) {
+    tasks = [];
+  } else {
+    tasks = JSON.parse(localStorage.getItem('tasks'));
+  }
+  tasks.forEach(function(task) {
+    // Create li element
+    const li = document.createElement('li');
+    li.className = 'collection-item';
+
+    // Create text node and append to li
+    li.appendChild(document.createTextNode(task));
+    // Create new link element
+    const link = document.createElement('a');
+    // Add class
+    link.className = 'delete-item secondary-content';
+    //Add icon html
+    link.innerHTML = '<i class="fa fa-remove"></i>';
+    //Append the link to li
+    li.appendChild(link);
+
+    //Append li to ul
+    taskList.appendChild(li);
+  });
 }
 
 //Add Task
@@ -53,7 +84,7 @@ function addTask(e) {
 
   //Store Task
   function storeTaskInLocalStorage(task) {
-    let task;
+    let tasks;
     if (localStorage.getItem('tasks') === null) {
       tasks = [];
     } else {
