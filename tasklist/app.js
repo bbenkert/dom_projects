062a8494aab2b039854320f6041b459e8b6f7831
Benjamin Benkert <ben@peakperformancepc.com>
2018-07-22 17:50:19 -0400

    Add remove task function and event listener

--- tasklist/app.js
+++ tasklist/app.js
@@ -11,33 +11,43 @@ loadEventListeners();
 function loadEventListeners() {
   // Add task Event
   form.addEventListener('submit', addTask);
+  //Remove task event
+  taskList.addEventListener('click', removeTask);
 }
 
 //Add Task
 function addTask(e) {
   if (taskInput.value === '') {
     alert('Add a task');
+  } else {
+    // Create li element
+    const li = document.createElement('li');
+    li.className = 'collection-item';
+
+    // Create text node and append to li
+    li.appendChild(document.createTextNode(taskInput.value));
+    // Create new link element
+    const link = document.createElement('a');
+    // Add class
+    link.className = 'delete-item secondary-content';
+    //Add icon html
+    link.innerHTML = '<i class="fa fa-remove"></i>';
+    //Append the link to li
+    li.appendChild(link);
+
+    //Append li to ul
+    taskList.appendChild(li);
+    //Clear input
+    taskInput.value = '';
+
+    e.preventDefault();
   }
-
-  // Create li element
-  const li = document.createElement('li');
-  li.className = 'collection-item';
-
-  // Create text node and append to li
-  li.appendChild(document.createTextNode(taskInput.value));
-  // Create new link element
-  const link = document.createElement('a');
-  // Add class
-  link.className = 'delete-item secondary-content';
-  //Add icon html
-  link.innerHTML = '<i class="fa fa-remove"></i>';
-  //Append the link to li
-  li.appendChild(link);
-
-  //Append li to ul
-  taskList.appendChild(li);
-  //Clear input
-  taskInput.value = '';
-
+  e.preventDefault();
+}
+// Remove task
+function removeTask(e) {
+  if (e.target.parentElement.classList.contains('delete-item')) {
+    e.target.parentElement.parentElement.remove();
+  }
   e.preventDefault();
 }
